12492a4fe53d563d151383df828f969ff8f984ed
KeithAverre <kmaverre@alaska.edu>
2022-07-12 11:10:44 -0800

    Commenting and initial commandline program information file added

--- accuracyTest.py
+++ accuracyTest.py
@@ -1,6 +1,7 @@
 import unittest
 
-#this will test if
+
+# this will test multiple specific and overall expected outputs of the program
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
---
+++ programInfoFile.py
@@ -0,0 +1,3 @@
+# This file includes all commandline built-in information
+def programInformation(argv):
+    return 0
